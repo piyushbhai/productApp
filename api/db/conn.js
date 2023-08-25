ab8d793403e3d275f8a236e4e8ad5b37b5b905f0
piyushbhai <prajapati.piyush16@gmail.com>
2023-08-26 01:12:17 +0530

    product fixing

--- api/db/conn.js
+++ api/db/conn.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const db = 'mongodb://localhost:27017/productApp';
+// const db = 'mongodb://localhost:27017/productApp';
+const db = 'mongodb+srv://piyush:PJMaBqUST8tN7YPB@productapp.tazel8g.mongodb.net/';
 
 mongoose.connect(db,{
     useNewUrlParser:true,
